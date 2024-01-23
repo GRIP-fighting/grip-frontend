02f3bfaa7c7420d185d0fdc2f374f1e11e1ef42d
Seungwan Kang <albertruaz@gmail.com>
2024-01-23 15:48:33 +0900

    fix: 기타 에러 수정

--- server/models/Map.js
+++ server/models/Map.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose"); // 몽구스를 가져온다.
 const Schema = mongoose.Schema;
+const { User } = require("../models/User.js"); // 모델 스키마 가져오기
 const { Counter } = require("./Counter.js");
 
 const mapSchema = mongoose.Schema({
@@ -44,17 +45,15 @@ mapSchema.pre("save", async function (next) {
                 { new: true, upsert: true }
             );
             map.mapId = counter.seq;
-            // for (const designerId of map.designer) {
-            //     const user = await User.findOne({ userId: designerId });
-            //     if (user) {
-            //         user.solutionId.push(map.mapId);
-            //         await user.save();
-            //     }
-            // }
-            await User.updateMany(
-                { UserId: { $in: map.designer } },
-                { $push: { solutionId: map.mapId } }
-            );
+            å;
+            try {
+                await User.updateMany(
+                    { userId: { $in: map.designer } },
+                    { $push: { mapId: map.mapId } }
+                );
+            } catch (error) {
+                next(error);
+            }
         }
         next();
     } catch (error) {
