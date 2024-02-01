986fcb7f0194ce7ae8b3c6052f7c4674787e0856
Seungwan Kang <albertruaz@gmail.com>
2024-02-02 03:27:20 +0900

    Remove server folder

--- server/config/key.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    ID: process.env.ID,
-    SECRET: process.env.SECRET,
-    BUCKET_NAME: process.env.BUCKET_NAME,
-};
